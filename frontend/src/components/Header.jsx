d03bc41d545619647d7471615ad3b688acdf524c
olearybmps <160647100+olearybmps@users.noreply.github.com>
2024-05-28 18:49:28 -0400

    Improved header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -15,33 +15,37 @@ function Header() {
                         className="header-image"
                     />
                 </div>
-                <h1>Recipes to Health & Wellness</h1>
-                {/* Navigation bar */}
-                <nav>
-                    <ul>
-                        <li>
-                            {/* Link to the Breakfast page */}
-                            <Link to="/">Breakfast</Link>
-                        </li>
-                        <li>
-                            {/* Link to the Lunch page */}
-                            <Link to="/lunch">Lunch</Link>
-                        </li>
-                        <li>
-                            {/* Link to the Dinner page */}
-                            <Link to="/dinner">Dinner</Link>
-                        </li>
-                        <li>
-                            {/* Link to the Snack page */}
-                            <Link to="/snack">Snacks</Link>
-                        </li>
-                        <li>
-                            {/* Link to the Community page */}
-                            <Link to="/user-recipes">Community<br />Recipes</Link>
-                        </li>
-                        {/* more links */}
-                    </ul>
-                </nav>
+                <div className="header-text">
+                    <h1>Recipes to Health & Wellness</h1>
+                    {/* Navigation bar */}
+                    <nav>
+                        <ul>
+                            <li>
+                                {/* Link to the Breakfast page */}
+                                <Link to="/">Breakfast</Link>
+                            </li>
+                            <li>
+                                {/* Link to the Lunch page */}
+                                <Link to="/lunch">Lunch</Link>
+                            </li>
+                            <li>
+                                {/* Link to the Dinner page */}
+                                <Link to="/dinner">Dinner</Link>
+                            </li>
+                            <li>
+                                {/* Link to the Snack page */}
+                                <Link to="/snack">Snacks</Link>
+                            </li>
+                            <li>
+                                {/* Link to the Community page */}
+                                <Link to="/user-recipes">
+                                    Community Recipes
+                                </Link>
+                            </li>
+                            {/* more links */}
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </header>
     );
